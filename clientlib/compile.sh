d893e1049251558b49811f0e5b666dc78b059754
nchudakov <n.chudakov@ventra.ru>
2015-03-30 11:57:57 +0300

    timers.js remove

--- clientlib/compile.sh
+++ clientlib/compile.sh
@@ -8,11 +8,8 @@ if [ ! -f compiler.jar ]; then
 fi
 
 if [ $1 == "u" ]; then
-  wget http://dl.google.com/closure-compiler/compiler-latest.zip -O compiler.zip
-  unzip compiler.zip
-  rm -f compiler.zip
-
-  rm jquery.js sweetalert.js flipclock.js flipclock.css sweetalert.css
+  #rm jquery.js sweetalert.js flipclock.js flipclock.css sweetalert.css socketio.js timer
+  wget https://raw.githubusercontent.com/bfattori/timersjs/master/timers.js -O timers.js
   wget http://code.jquery.com/jquery-2.1.3.min.js -O jquery.js
   wget https://raw.githubusercontent.com/Automattic/socket.io-client/master/socket.io.js -O socketio.js
   wget https://raw.githubusercontent.com/t4t5/sweetalert/master/lib/sweet-alert.js -O sweetalert.js
@@ -22,7 +19,7 @@ if [ $1 == "u" ]; then
 fi
 
 echo '(function() {' > alljs.js
-cat jquery.js sweetalert.js timer.js socketio.js flipclock.js client.js >> alljs.js
+cat jquery.js sweetalert.js timers.js socketio.js flipclock.js client.js >> alljs.js
 echo '})();' >> alljs.js
 java -jar compiler.jar alljs.js --js_output_file compiled.js --compilation_level WHITESPACE_ONLY --output_wrapper "var remoterpc = {properties: {username: 'unnamed',project: 'hrb'}};%output%"
 rm alljs.js
